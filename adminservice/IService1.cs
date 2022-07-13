95efda273cf4bac4bc2d146fee24870b655de933
pankajy2 <pankajy2@CHETUIWK1857>
2022-07-13 14:41:34 +0530

    Add project files.

---
+++ adminservice/IService1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace adminservice
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        List<string> LoginUserDetails(UserDetails userInfo);
+
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "adminservice.ContractType".
+
+        [DataContract]
+        public class UserDetails
+        {
+            string email = string.Empty;
+            string password = string.Empty;
+            //string empid;
+
+            [DataMember]
+            public string Email
+            {
+                get { return email; }
+                set { email = value; }
+            }
+            [DataMember]
+            public string Password
+            {
+                get { return password; }
+                set { password = value; }
+            }
+            
+        }
+    
+}
---
+++ adminservice/Service1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace adminservice
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class Service1 : IService1
+    {
+        string connection = ConfigurationManager.ConnectionStrings["xyz"].ConnectionString;
+        public List<string> LoginUserDetails(UserDetails userInfo)
+        {
+
+            List<string> user = new List<string>();
+            
+            SqlConnection con = new SqlConnection(connection);
+            con.Open();
+            SqlCommand cmd = new SqlCommand("loginsert", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@email", userInfo.Email);
+            cmd.Parameters.AddWithValue("@password", userInfo.Password);         
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read() == true)
+            {
+                user.Add(dr[0].ToString());               
+            }
+            con.Close();
+            return user;
+        }
+       
+        
+    }
+}
